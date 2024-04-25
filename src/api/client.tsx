7d6ac67afa261c8045124ab7a5f363a96beb5664
Iv3tt3 <reach.iv3tt3@gmail.com>
2024-04-25 18:35:33 +0100

    env url

--- src/api/client.tsx
+++ src/api/client.tsx
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const baseURL = import.meta.env.REACT_APP_API_BASE_URL as string
+
 export const client = axios.create({
-  baseURL: 'http://localhost:3001/',
+  baseURL,
 });
 
 client.interceptors.response.use(response => response.data);
