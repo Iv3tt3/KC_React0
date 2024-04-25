e93da23d608d617ba84c49fe2d1ac61c64bf046b
Iv3tt3 <reach.iv3tt3@gmail.com>
2024-04-25 19:42:46 +0100

    get request header auth

--- src/pages/adverts/AdvertsList.tsx
+++ src/pages/adverts/AdvertsList.tsx
@@ -1,5 +1,8 @@
+import { IAdvert } from '../../utils/interfaces';
 import Advert from './components/Advert';
 import './components/Advert.modules.css';
+import { getAdverts } from './service';
+import { useEffect, useState } from 'react';
 
 const example = [{
     "id": "9a7a5614-871e-4506-a332-61adcc19c4cf",
@@ -26,14 +29,28 @@ const example = [{
 
 export function AdvertsList() {
 
+    const [adverts, setAdverts] = useState<IAdvert[]>([]);
+
+    useEffect(() => {
+        getAdverts().then(ads => setAdverts(ads));
+    },[])
+
+
     return (
         <div>
+            <div>
+            {adverts.map(({...advert}) => (
+                <li key={advert.id}>
+                    <Advert{...advert}/>
+                </li>
+                ))};
+            </div>
             <ul className="advert-list">
                 {example.map(({...example }) => (
                 <li className="advert-container" key={example.id}>
                     <Advert {...example}/>
                 </li>
-                ))}
+                ))};
             </ul>
         </div>
     );
--- src/pages/adverts/service.tsx
+++ src/pages/adverts/service.tsx
@@ -1,10 +1,18 @@
 import { client } from "../../api/client";
 import { IAdvert } from "../../utils/interfaces";
+import storage from "../../utils/storage";
 
 const advertsURL = '/api/v1/adverts'
 
+
+
 export const getAdverts = (): Promise<Array<IAdvert>> => {
-    const url = `${advertsURL}`;
-    return client.get(url);
-  };
+  const url = `${advertsURL}`;
+  const token = storage.get('key')
+  return client.get(url, {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }}
+  )
+};
   
