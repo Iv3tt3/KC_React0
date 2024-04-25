5c9a1613e3ff4c86ce5527cc8a31bb3c2087ac8b
Iv3tt3 <reach.iv3tt3@gmail.com>
2024-04-25 17:57:00 +0100

    button styled-component styles

--- src/componentes/shared/Button.tsx
+++ src/componentes/shared/Button.tsx
@@ -2,20 +2,20 @@ import styled from 'styled-components';
 
 
 const Button = styled.button`
-cursor: pointer;
-border-radius: 9999px;
-border-style: solid;
-border-width: 1px;
-display: inline-flex;
-align-items: center;
-font: inherit;
-font-weight: bold;
-min-height: 36px;
-justify-content: center;
-min-width: 72px;
-outline-style: none;
-text-decoration: none;
-transition: background-color 0.2s;
+    border: none;
+    color: white;
+    padding: 8pt 30pt;
+    cursor: pointer;
+    border-radius: 3px;
+    background-color: #0056b3; 
+    transition: transform 0.3s ease-in-out;
+    &:hover {
+        background-color: lightgrey;
+        color:black;
+    }
+    &:active {
+        transform: scale(0.90);
+    }
 `;
 
 export default Button;
