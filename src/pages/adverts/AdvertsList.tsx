8820f7cc16cf3b8b7e12634e228a6b26c8de09f1
Iv3tt3 <reach.iv3tt3@gmail.com>
2024-04-30 12:43:45 +0100

    styles CSS module

--- src/pages/adverts/AdvertsList.tsx
+++ src/pages/adverts/AdvertsList.tsx
@@ -1,31 +1,10 @@
 import { IAdvert } from '../../utils/interfaces';
 import Advert from './components/Advert';
-import './components/Advert.modules.css';
+import styles from './components/Advert.module.css';
 import { getAdverts } from './service';
 import { useEffect, useState } from 'react';
 
-const example = [{
-    "id": "9a7a5614-871e-4506-a332-61adcc19c4cf",
-    "createdAt": "2024-04-24T18:50:44.000Z",
-    "name": "Bike",
-    "sale": true,
-    "price": 200,
-    "tags": [
-        "lifestyle"
-    ],
-    "photo": "http://localhost:3001/public/1713984644031-437908861.jpeg"
-    },{
-    "id": "9a7a5614f",
-    "createdAt": "2024-04-24T18:50:44.000Z",
-    "name": "Bike",
-    "sale": true,
-    "price": 200,
-    "tags": [
-        "lifestyle"
-    ],
-    "photo": "http://localhost:3001/public/1713984644031-437908861.jpeg"
-    }]
-
+console.log(styles)
 
 export function AdvertsList() {
 
@@ -38,19 +17,12 @@ export function AdvertsList() {
 
     return (
         <div>
-            <div>
+            <ul className={styles.advertsList}>
             {adverts.map(({...advert}) => (
-                <li key={advert.id}>
-                    <Advert{...advert}/>
+                <li className={styles.advertContainer} key={advert.id}>
+                    <Advert {...advert}/>
                 </li>
-                ))};
-            </div>
-            <ul className="advert-list">
-                {example.map(({...example }) => (
-                <li className="advert-container" key={example.id}>
-                    <Advert {...example}/>
-                </li>
-                ))};
+                ))};     
             </ul>
         </div>
     );
--- src/pages/adverts/components/Advert.tsx
+++ src/pages/adverts/components/Advert.tsx
@@ -1,13 +1,14 @@
 import { IAdvert } from "../../../utils/interfaces";
+import styles from './Advert.module.css';
 
 export default function Advert ({id, name, sale, price, tags, photo }:IAdvert) {
     return (
         <>
-        <p className="advert-name">{name}</p>
-        <p className="advert-sale">{sale}</p>
-        <p className="advert-price">{price}</p>
-        <p className="advert-tags">{tags.toString()}</p>
-        <img className="advert-img" src={photo}/>
+        <p className={styles.advertName}>{name}</p>
+        <p className={styles.advertSale}>{sale}</p>
+        <p className={styles.advertPrice}>{price}</p>
+        <p className={styles.advertTags}>{tags.toString()}</p>
+        <img className={styles.advertImg} src={photo}/>
         </>
     );
 }
