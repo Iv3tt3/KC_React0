46879d27a1d3dddd9bff548ab937f8180c7bfba1
Iv3tt3 <reach.iv3tt3@gmail.com>
2024-05-02 16:07:24 +0100

    input control

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { ReactEventHandler, useState } from "react";
 import Button from "../../componentes/shared/Button";
 import { login } from "./service";
 
@@ -9,11 +9,6 @@ export function LoginPage({onLogged}: { onLogged: () => void }) {
 
     const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-
-        const formData = new FormData(event.currentTarget);
-        const email = formData.get('email') as string;
-        const password = formData.get('password') as string;
-
         onLogged()
 
         await login ({
@@ -22,15 +17,21 @@ export function LoginPage({onLogged}: { onLogged: () => void }) {
         });
     }
 
+    const handleEmChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setEmail(event.target.value)
+    }
+
+    const handlePwChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setPassword(event.target.value)
+    }
+
     return (
     <div>
         <h1>Login to your account</h1>
         <form onSubmit={handleSubmit}>
-            <input type="text" name="email" value={email} onChange= {event => {
-                setEmail(event.target.value)}}
+            <input type="text" name="email" value={email} onChange= {handleEmChange}
             />
-            <input type="password" name="password" value={password} onChange= {event => {
-                setPassword(event.target.value)}}/>
+            <input type="password" name="password" value={password} onChange= {handlePwChange}/>
             <Button type="submit" disabled={!email || !password}>Log in</Button>
         </form>
     </div>
