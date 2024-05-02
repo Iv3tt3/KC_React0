dc6d629f528d6e9fe38bacead4ecd81583b188ef
Iv3tt3 <reach.iv3tt3@gmail.com>
2024-05-02 22:35:47 +0100

    fix bug in AuthorizationHeader

--- src/api/client.tsx
+++ src/api/client.tsx
@@ -3,11 +3,10 @@ import axios from 'axios';
 const baseURL = import.meta.env.VITE_BASE_URL
 
 export const client = axios.create({
-  baseURL,
+  baseURL
 });
 
 client.interceptors.response.use(response => response.data);
 
-export const setAuthorizationHeader = (token:string) => {
+export const setAuthorizationHeader = (token:string) => 
   (client.defaults.headers.common['Authorization'] = `Bearer ${token}`);
-}
--- src/pages/adverts/service.tsx
+++ src/pages/adverts/service.tsx
@@ -1,15 +1,10 @@
-import { client, setAuthorizationHeader } from "../../api/client";
+import { client} from "../../api/client";
 import { IAdvert } from "../../utils/interfaces";
-import storage from "../../utils/storage";
 
-const advertsURL = '/api/v1/adverts'
-
-
+const advertsURL = '/api/v1/adverts';
 
 export const getAdverts = (): Promise<Array<IAdvert>> => {
   const url = `${advertsURL}`;
-  const token = storage.get('key') // this should work with axios
-  setAuthorizationHeader(token) //this should work with axios
-  return client.get(url)
+  return client.get(url);
 };
   
