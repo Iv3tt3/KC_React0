acd5a46c56bb0e016fc950d6d510394ffe86a944
Iv3tt3 <reach.iv3tt3@gmail.com>
2024-05-03 11:45:10 +0100

    default logged if check

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import { useState } from "react"
 import { AdvertsList } from "./pages/adverts/AdvertsList"
 import { LoginPage } from "./pages/auth/Login"
 
-function App(isDefaultLogged: any) {
+function App({isDefaultLogged}: { isDefaultLogged: boolean }) {
+  
   const [isLogged, setIsLogged] = useState (isDefaultLogged);
-
+  
   const handleIsLogged = () => setIsLogged(true);
 
   const handleIsLogout = () => setIsLogged(false)
-
+  
   return (
     <>{isLogged 
     ? <AdvertsList onLogout={handleIsLogout}/>
--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,24 +1,27 @@
-import React, { ReactEventHandler, useState } from "react";
+import React, {useState } from "react";
 import Button from "../../componentes/shared/Button";
 import { login } from "./service";
 
-export function LoginPage({onLogged}: { onLogged: () => void }) {
+export function LoginPage(
+    {onLogged}: { onLogged: () => void }) {
 
     const [formData, setFormData] = useState({
         email: '',
         password: ''
     })
 
+    const [checked, setChecked] = useState(false)
+
     const {email, password} = formData
 
     const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         onLogged()
-
+        if (checked) {
         await login ({
             email,
             password,
-        });
+        });}
     }
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -28,13 +31,17 @@ export function LoginPage({onLogged}: { onLogged: () => void }) {
         }));
     }
 
+    const handleCheck = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setChecked(event.target.checked)
+    }
+
     return (
     <div>
         <h1>Login to your account</h1>
         <form onSubmit={handleSubmit}>
-            <input type="text" name="email" value={email} onChange= {handleChange}
-            />
+            <input type="text" name="email" value={email} onChange= {handleChange}/>
             <input type="password" name="password" value={password} onChange= {handleChange}/>
+            <input type="checkbox" checked={checked} onChange={handleCheck}/>
             <Button type="submit" disabled={!email || !password}>Log in</Button>
         </form>
     </div>
