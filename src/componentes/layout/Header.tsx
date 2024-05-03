06656c9d240dfe2f527dbf2146b51e63f63fa4b8
Iv3tt3 <reach.iv3tt3@gmail.com>
2024-05-03 14:25:03 +0100

    Header Footer Layout

---
+++ src/componentes/layout/Header.tsx
@@ -0,0 +1,13 @@
+import { Logo } from "../shared/logo";
+
+
+export default function Header(){
+    return (
+        <header>
+        <div>
+            <Logo></Logo>
+        </div>
+        <nav></nav>
+        </header>
+    );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import storage from './utils/storage.tsx';
 import { setAuthorizationHeader } from './api/client.tsx';
+import Layout from './componentes/layout/Layout.tsx';
 
 const accessToken = storage.get('key');
 if (accessToken) {
@@ -11,6 +12,8 @@ if (accessToken) {
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   //<React.StrictMode>
+  <Layout>
     <App isDefaultLogged={!!accessToken} /> 
+  </Layout>
   //</React.StrictMode>,
 )
